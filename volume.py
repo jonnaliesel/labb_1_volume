bc0e7c12f14966f92c887867cece69baf2f4b0f1
jonnaliesel <jonnaliesel@gmail.com>
2021-09-15 17:50:44 +0200

    add line braks for readability

--- volume.py
+++ volume.py
@@ -7,14 +7,14 @@ figure = input("Hey, let's play with some Geometry!\nWanna get the volume of a c
 
 while figure :
     if (figure == 'c') :
-        side = int(input('How many centimeters is each side of your cube? '))
+        side = int(input('\nHow many centimeters is each side of your cube? '))
         volume = side**3
-        print('Cool! The volume of your cube with the side length of ' + str(side) + ' is equal to ' + str(volume) + ' cm3 or ' + str(volume/1000) + ' liters')
+        print('\nCool! The volume of your cube with the side length of ' + str(side) + ' is equal to ' + str(volume) + ' cm3 or ' + str(volume/1000) + ' liters')
     elif figure == 't' :
-        side = int(input('How many centimeters is each side of your equilateral tetrahedron? '))
+        side = int(input('\nHow many centimeters is each side of your equilateral tetrahedron? '))
         volume = int(math.sqrt(2)*side**3/12)
-        print('Cool! The volume of your tetrahedron with the side length of ' + str(side) + ' is equal to ' + str(volume) + ' cm3 or ' + str(volume/1000) + ' liters')
+        print('\nCool! The volume of your tetrahedron with the side length of ' + str(side) + ' is equal to ' + str(volume) + ' cm3 or ' + str(volume/1000) + ' liters')
     else :
-        figure = input('Sorry, your input is not valid. Try again :)\nc/t : ')
-    figure = input('Cool, wanna try again? :) Enter cube or tetrahedon c/t : ')
+        figure = input('\nSorry, your input is not valid. Try again :)\nc/t : ')
+    figure = input('Wanna try again? :) Enter cube or tetrahedon c/t : ')
     
